3c8c773bb0e8ba30ef588108b776ffd29b0beb38
TopSecretAgent007 <lendonbracewell1114@gmail.com>
2024-10-30 09:39:44 +0900

    Update dealOrder.ts

--- src/dedust/dealOrder.ts
+++ src/dedust/dealOrder.ts
@@ -21,38 +21,6 @@ export async function dealOrder(){
             })
         );
         let sender = await wallet.sender(keyPair.secretKey);
-        if(user.orderingData)
-            user.orderingData!.map(async (order) => {
-                //mainCoin refers to the coin what I have and want to exchange.
-                const pool = await getPoolWithCaption(order.jettons);
-                const mainCoinId : number = order.isBuy ? order.mainCoin : 1 - order.mainCoin;
-                const fromJetton : string = order.jettons[mainCoinId]!;
-                const fromAddress : string = pool!.assets[mainCoinId]!.replace('jetton:','');
-                const toJetton : string = order.jettons[1- mainCoinId]!;
-                const toAddress : string = pool!.assets[1- mainCoinId]!.replace('jetton:','');
-                const amount = BigInt(Math.floor(10 ** pool?.decimals[mainCoinId]! * order.amount));//unit in ton fo rton=>jetto
-                //ton_to_jetton case
-                try {
-                    console.log('start tx');
-                    const pricePost = await fetchPrice(10 **  pool!.decimals[1 - mainCoinId]!, pool!.assets[1- mainCoinId]!, pool!.assets[mainCoinId]!);
-                    //compare price and send tx , delete document.
-
-                    if(pricePost * (order.isBuy ? 1 : -1) <= order.price * 10 ** pool!.decimals[mainCoinId]! * (order.isBuy ? 1 : -1)){
-                        if(fromJetton == "TON"){
-                            await ton_to_Jetton(sender, Address.parse(toAddress), amount);
-                        } else if(toJetton == "TON"){
-                            await jetton_to_Ton(sender, wallet.address, Address.parse(fromAddress), amount);
-                        } else {
-                            await jetton_to_Jetton(sender, wallet.address, Address.parse(fromAddress), Address.parse(toAddress), amount);
-                        }
-                        bot.sendMessage(user.telegramID, 'TX realised, Visit https://tonviewer.com/' + user.walletAddress);
-                        deleteOrderingDataFromUser(user.telegramID,order!._id);
-                    }
-                } catch (error) {
-                    console.log(error)
-                }
-                
-            })
     })
     console.log('==>dealing order Finished<==');
 
